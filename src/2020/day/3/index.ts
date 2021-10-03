f0f72150a616a1dc173dc88f6de2903daebcc443
Ernest Landrito <landrito@google.com>
2021-10-03 00:08:58 -0600

    Day 3 Part 2

--- src/2020/day/3/index.ts
+++ src/2020/day/3/index.ts
@@ -1,7 +1,12 @@
 /*
-With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+With the toboggan login problems resolved, you set off toward the airport. While
+travel by toboggan might be easy, it's certainly not safe: there's very minimal
+steering and the area is covered in trees. You'll need to see which angles will
+take you near the fewest trees.
 
-Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+Due to the local geology, trees in this area only grow on exact integer
+coordinates in a grid. You make a map (your puzzle input) of the open squares
+(.) and trees (#) you can see. For example:
 
 ..##.......
 #...#...#..
@@ -14,7 +19,9 @@ Due to the local geology, trees in this area only grow on exact integer coordina
 #.##...#...
 #...##....#
 .#..#...#.#
-These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+These aren't the only trees, though; due to something you read about once
+involving arboreal genetics and biome stability, the same pattern repeats to the
+right many times:
 
 ..##.........##.........##.........##.........##.........##.......  --->
 #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
@@ -27,13 +34,19 @@ These aren't the only trees, though; due to something you read about once involv
 #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
 #...##....##...##....##...##....##...##....##...##....##...##....#
 .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+You start on the open square (.) in the top-left corner and need to reach the
+bottom (below the bottom-most row on your map).
 
-The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+The toboggan can only follow a few specific slopes (you opted for a cheaper
+model that prefers rational numbers); start by counting all the trees you would
+encounter for the slope right 3, down 1:
 
-From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+From your starting position at the top-left, check the position that is right 3
+and down 1. Then, check the position that is right 3 and down 1 from there, and
+so on until you go past the bottom of the map.
 
-The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+The locations you'd check in the above example are marked here with O where
+there was an open square and X where there was a tree:
 
 ..##.........##.........##.........##.........##.........##.......  --->
 #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
@@ -46,37 +59,79 @@ The locations you'd check in the above example are marked here with O where ther
 #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
 #...##....##...##....##...#X....##...##....##...##....##...##....#
 .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-In this example, traversing the map using this slope would cause you to encounter 7 trees.
+In this example, traversing the map using this slope would cause you to
+encounter 7 trees.
 
-Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+Starting at the top-left corner of your map and following a slope of right 3 and
+down 1, how many trees would you encounter?
+
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability of a
+sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following
+slopes, you start at the top-left corner and traverse the map all the way to the
+bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each
+of the listed slopes?
 */
 
-import { lines } from "../../../input-helpers";
+import {lines} from '../../../input-helpers';
 
-const INPUT_FILE = '/Users/landrito/Code/advent-of-code/src/2020/day/3/input.txt';
+const INPUT_FILE =
+    '/Users/landrito/Code/advent-of-code/src/2020/day/3/input.txt';
 
-async function day3(){
+interface Slope {
+  xOffset: number, yOffset: number,
+}
+
+async function countTrees({xOffset, yOffset}: Slope) {
   let x = 0;
+  let y = 0;
   let count = 0;
 
   for await (const line of lines(INPUT_FILE)) {
-    const char = line.charAt(x % line.length);
-    switch(char) {
-      case '#':
-        count = count + 1;
-        break;
-     case '':
-        return count;
-      default:
-        break;
-    }  
-    x = x+3;
+    if (y % yOffset === 0) {
+      const char = line.charAt(x % line.length);
+      switch (char) {
+        case '#':
+          count = count + 1;
+          break;
+        default:
+          break;
+      }
+      x = x + xOffset;
+    }
+    y = y + 1;
   }
   return count;
 }
 
 async function main() {
-  console.log(await(day3()));
+  console.log('Part 1');
+  console.log(await (countTrees({xOffset: 3, yOffset: 1})));
+
+  console.log('Part 2');
+  const part2Result = [
+    {xOffset: 1, yOffset: 1},
+    {xOffset: 3, yOffset: 1},
+    {xOffset: 5, yOffset: 1},
+    {xOffset: 7, yOffset: 1},
+    {xOffset: 1, yOffset: 2},
+  ].reduce(async (product: Promise<number>, slope: Slope) => {
+    const result = await countTrees(slope);
+    return await product * result;
+  }, Promise.resolve(1));
+  console.log(await part2Result);
 }
 
 main();
