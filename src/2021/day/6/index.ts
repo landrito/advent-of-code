24940cfc95e7de9af98374609a03792492dba9f3
landrito <landrito@google.com>
2021-12-06 16:46:43 -0700

    2021 day 6

---
+++ src/2021/day/6/index.ts
@@ -0,0 +1,39 @@
+import {program} from '../../../program';
+
+function partOne([input]: Array<string>): number {
+  let numbers = input.split(',').map(s => parseInt(s));
+  return solution(numbers, 80);
+}
+
+function partTwo([input]: Array<string>): number {
+  let numbers = input.split(',').map(s => parseInt(s));
+  return solution(numbers, 256);
+}
+
+interface NumberCount {
+  [n: number]: number
+}
+
+function solution(numbers: Array<number>, days: number): number {
+  let counts: NumberCount = {};
+  for (let i = 0; i <= 8; i++) {
+    counts[i] = 0;
+  }
+  counts =
+      numbers.reduce((counts, v) => ({...counts, [v]: counts[v] + 1}), counts);
+
+  const _ = Array(9).fill(null);
+  for (let i = 0; i < days; i++) {
+    counts = _.reduce(
+        (nc, _, index) => ({
+          ...nc,
+          ...(index === 8     ? {8: counts[0]} :
+                  index === 6 ? {6: counts[7] + counts[0]} :
+                                {[index]: counts[index + 1]})
+        }),
+        {} as NumberCount)
+  }
+  return Object.values(counts).reduce((x, y) => x + y, 0)
+}
+
+program(partOne, partTwo);
